c2e90b1df3b96695d1ed89535456e36250135bb4
Rokami <Rokami@users.noreply.github.com>
2017-08-14 16:02:54 +0800

    Update AsmN50.pl

--- AsmN50.pl
+++ AsmN50.pl
@@ -4,8 +4,8 @@ if(!$ARGV[0])
 {
     print <<END;
     usage:perl AsmN50.pl <scaffold.fasta> [repeat.gff3]
-          if only supply scaffold.fasta,exec will calcuate ATGCN percentage and N50;
-          if reapeat.gff3 supplied,exec will calcuate percentage of Repeat percentage and output 
+          if only supply scaffold.fasta,exec will calculate ATGCN percentage and N50;
+          if reapeat.gff3 supplied,exec will calculate percentage of Repeat percentage and output 
           overlap of each class of repeat to log.txt.
           and transfer Repeat sequence to lowchar,output to <scaffold.fasta>.transfer.fasta;
 END
